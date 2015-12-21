0669b05b07baead5ba40b86002a0d412a86be5d3
zengjc <11607150@qq.com>
2015-12-21 18:41:23 +0800

    线程测试

---
+++ src/testpython/sched/thread2.py
@@ -0,0 +1,32 @@
+'''
+Created on 2015年12月21日
+
+@author: zjc
+'''
+#coding=utf-8
+import threading
+from time import ctime,sleep
+
+
+def music(func):
+    for i in range(2):
+        print ("I was listening to %s.%s" %(func,ctime()))
+        sleep(1)
+
+def move(func):
+    for i in range(2):
+        print ("I was at the %s! %s" %(func,ctime()))
+        sleep(5)
+
+threads = []
+t1 = threading.Thread(target=music,args=('爱情买卖',))
+threads.append(t1)
+t2 = threading.Thread(target=move,args=('阿凡达',))
+threads.append(t2)
+
+if __name__ == '__main__':
+    for t in threads:
+        t.setDaemon(True)
+        t.start()
+
+    print ("all over %s" %ctime())
